82fa45ca0c31133d5ce9e2bdc36423b6f3ea4ea4
Aaron Yang <ay2707@princeton.edu>
2024-10-03 03:53:18 -0400

    oops infinite loop again

--- replace.c
+++ replace.c
@@ -37,7 +37,7 @@ static size_t replaceAndWrite(const char *pcLine,
    curr = (char*) pcLine;
    while (Str_search(curr, pcFrom) != NULL) {
       numReplaced++;
-      while (curr != Str_search(pcLine, pcFrom)) {
+      while (curr != Str_search(curr, pcFrom)) {
          printf("%c", *curr);
          curr++;
       }
